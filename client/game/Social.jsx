8a8163ae42893a977b10b61a6f44c7f35a834e1f
Samuel Dupret <samu.dupret@hotmail.com>
2021-06-09 11:30:35 +0100

    just faces in social phase

--- client/game/Social.jsx
+++ client/game/Social.jsx
@@ -7,9 +7,9 @@ export default class Social extends Component {
 
         // Calculate summary of ratings
         const otherRatings = otherPlayers.map(p => p.get("ratings")[round.get("roundIndex")]).filter(rating => rating !== "NA")
-        const min = Math.min(...otherRatings).toFixed(2)
+        // const min = Math.min(...otherRatings).toFixed(2)
+        // const max = Math.max(...otherRatings).toFixed(2)
         const mean = (otherRatings.reduce((total, item) => (total += item), 0) / otherRatings.length).toFixed(2)
-        const max = Math.max(...otherRatings).toFixed(2)
 
         // Get the path
         const stimConfig = round.get("stimConfig")
@@ -20,24 +20,17 @@ export default class Social extends Component {
                 <p>
                     Please see below for the ratings given by {otherPlayers.length} other participants in this session. They saw the exact same group of faces as you did, and these ratings are real-time data.
                 </p>
-                <div className="social-holder">
-                    <div className="social-summary title">
-                        <p><strong>Summary:</strong></p>
-                        <p>Min of other players: <strong>{min}</strong></p>
-                        <p>Mean of other players: <strong>{mean}</strong></p>
-                        <p>Max of other players: <strong>{max}</strong></p>
-                        <br />
-                        <div>
-                            <p><strong>Average face created from the ratings of the other players:</strong></p>
-                            <img src={path} alt="image of morphed face" />
-                        </div>
-                    </div>
-                    <div className="social-responses">
-                        <p className="title"><strong>Other players' responses:</strong></p>
-                        {otherPlayers.map((p, i) => {
-                            return <SocialResponse key={i} p={p} {...this.props} />
-                        })}
-                    </div>
+                <br />
+                <div className="social-summary title">
+                    <p><strong>Average face created from the ratings of the other players:</strong></p>
+                    <img src={path} alt="image of morphed face" />
+                </div>
+                <br />
+                <p className="title"><strong>Other players' responses:</strong></p>
+                <div className="social-responses">
+                    {otherPlayers.map((p, i) => {
+                        return <SocialResponse key={i} p={p} stimConfig={stimConfig} index={i} {...this.props} />
+                    })}
                 </div>
                 <br />
                 {
@@ -52,11 +45,13 @@ export default class Social extends Component {
 
 class SocialResponse extends Component {
     render() {
-        const { p, round } = this.props
+        const { p, index, round, stimConfig } = this.props
+
+        const path = `stimuli/${stimConfig.person}${-1 + p.get("ratings")[round.get("roundIndex")] + stimConfig.range[0]}.jpg`
         return (
-            <div className="social-response">
-                <img src={p.get("avatar")} alt="avatar" className="social-response-avatar" />
-                <p>response: <strong>{p.get("ratings")[round.get("roundIndex")]}</strong></p>
+            <div className="social-response title">
+                <p>Player {index + 1}</p>
+                <img src={path} alt="other-player-response" />
             </div>
         )
     }
