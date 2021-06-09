536946562a68b99370fb313aecab2b7df5029e27
Samuel Dupret <samu.dupret@hotmail.com>
2021-06-09 17:23:00 +0100

    improve end of practice

--- client/game/PracticeEnd.jsx
+++ client/game/PracticeEnd.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export default class PracticeEnd extends Component {
     render() {
+        const { player } = this.props
         return (
             <div>
-                This is the end of the practice round. Now you will do the actual round where you will be show the face array 3 times instead of two.
+                <p>
+                    This is the end of the practice round. Now you will do the actual round where you will be show the face array <strong>three times</strong> instead of two.
+                </p>
+                {
+                    player.stage.submitted
+                        ? <div className="title waiting-msg">Waiting for the other players...</div>
+                        : <div className="flex-c"><button className="main-btn" onClick={() => player.stage.submit()}>Continue</button></div>
+                }
             </div>
         )
     }
