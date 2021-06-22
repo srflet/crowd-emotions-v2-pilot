3a9a32d5c6967b476f263fbc61a31f17e873d013
Samuel Dupret <samu.dupret@hotmail.com>
2021-06-22 17:47:56 +0100

    add rating number in timeline

--- client/game/timeline/Timeline.jsx
+++ client/game/timeline/Timeline.jsx
@@ -5,11 +5,16 @@ export default class Timeline extends Component {
     render() {
         const { round, stage, player, game } = this.props
 
+        let ratingCount = -1
+        const ratingDescriptors = ["1st", "2nd", "3rd"]
+
         return (
             <div className="timeline">
                 <p className="title"><strong>{round.get("isPractice") ? `Practice Round` : `Round ${round.get("roundIndex")}/${game.treatment.nRounds}`}</strong></p>
                 {round.stages.map((s, index) => {
-                    return <p key={index} className={`${stage._id === s._id ? "current" : "notcurrent"}-stage`}>{s.displayName}</p>
+                    if (s.name === "rating") { ratingCount++ }
+
+                    return <p key={index} className={`${stage._id === s._id ? "current" : "notcurrent"}-stage`}>{s.name === "rating" && ratingDescriptors[ratingCount] + " "}{s.displayName}</p>
                 })}
                 <div className="my-timer">
                     <p>Timer</p>
