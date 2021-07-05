6d6306925f71b3bf8f6802323900e13b78a7281a
Samuel Dupret <samu.dupret@hotmail.com>
2021-07-05 10:53:45 +0100

    modify messages

--- client/intro/consent/WarningConsent.jsx
+++ client/intro/consent/WarningConsent.jsx
@@ -33,10 +33,10 @@ export default class WarningConsent extends Component {
                             <p><strong>The study lasts {introInfo.time}mins and pays between £{introInfo.flatPay} and £{introInfo.bonusPay} depending on your performance.</strong></p>
 
                             <p>This study involves groups of 10 participants. You will be given instructions before joining 9 other players in the lobby.</p>
-
                             <p>
-                                <strong>We need groups of 10 players for the study to begin.</strong> If 10 players show up in a lobby, then the study will start. Otherwise, you will wait up to {introInfo.timeout}mins in the lobby. If there are less than 10 players by the end of that time, the study will stop. You will be asked to return your participation on Prolific (this has no adverse effect on your Prolific score) and you will be paid £{introInfo.pityPay} for your time.
+                                <strong>We need groups of 10 players for the study to begin.</strong> If 10 players show up in a lobby, then the study will start. Otherwise, you will wait up to {introInfo.timeout}mins in the lobby. If there are less than 10 players by the end of that time, the study will stop. If the groups are full you will be informed, as they might fill up faster than expected. In both cases, you will be asked to return your participation on Prolific (this has no adverse effect on your Prolific score) and you will be paid £{introInfo.pityPay} for your time.
                             </p>
+
 
                             <div className="flex-c">
                                 <button
